f6f3e53abb66801855002cad4331094edec9298e
paul-ventura-git <j.paul.ventura@gmail.com>
2026-03-11 11:31:46 -0500

    First commit

---
+++ db.js
@@ -0,0 +1,9 @@
+export const users = [
+  { id: "1", name: "Ana", city: "Lima" },
+  { id: "2", name: "Luis", city: "Arequipa" }
+];
+
+export const orders = [
+  { id: "101", userId: "1", total: 120 },
+  { id: "102", userId: "2", total: 90 }
+];
---
+++ server.js
@@ -0,0 +1,77 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import axios from 'axios';
+import { users, orders } from './db.js';
+import fs from 'fs';
+
+// -------- SCHEMA ----------
+const typeDefs = `#graphql
+  type User {
+    id: ID
+    name: String
+    city: String
+    orders: [Order]
+  }
+
+  type Order {
+    id: ID
+    total: Int
+  }
+
+  type Post {
+    id: ID
+    title: String
+  }
+
+  type Config {
+    appName: String
+    version: String
+  }
+
+  type Query {
+    users: [User]
+    user(id: ID!): User
+    posts: [Post]
+    config: Config
+  }
+`;
+
+// -------- RESOLVERS ----------
+const resolvers = {
+  Query: {
+
+    users: () => users,
+
+    user: (_, { id }) => users.find(u => u.id === id),
+
+    posts: async () => {
+      const res = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts?_limit=3"
+      );
+      return res.data;
+    },
+
+    config: () => {
+      const raw = fs.readFileSync("./config.json");
+      return JSON.parse(raw);
+    }
+  },
+
+  User: {
+    orders: (user) =>
+      orders.filter(o => o.userId === user.id)
+  }
+};
+
+// -------- SERVER ----------
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+// 👇 forma correcta en Apollo v4
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 }
+});
+
+console.log(`🚀 Server ready at ${url}`);
